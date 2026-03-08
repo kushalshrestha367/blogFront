d0521a90982557fadc126c55d1c3b9a8feb45532
labirt niar <labirtniar@gmail.com>
2026-03-08 14:06:53 +0545

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import Card from '../components/Card'
+import axios from 'axios'
+
+function Home() {
+    const [blogs, setBlogs] = useState([])
+    //from backend
+    const fetchBlogs = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/blog')
+            const data = response.data.data
+            setBlogs(data)
+        }
+        catch (error) {
+            console.error('Error fetching blogs:', error)
+        }
+    }
+    useEffect(() => {
+        fetchBlogs()
+    }, [])
+
+  return (
+    <div>
+        <Navbar/>
+        <div className='flex flex-wrap justify-center gap-4 mt-4'>
+            {
+                blogs.map((blog) => (
+                    // <Card key={blog._id} title={blog.title} subtitle={blog.subtitle} description={blog.description} image={blog.image} />
+                    <Card blog={blog}/>
+                ))
+            }
+        
+        </div>
+     
+    </div>
+  )
+}
+
+export default Home
